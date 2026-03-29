5a25d06775805ba70f14934535c0fb7c67677c42
wengel-hm <william.engel@mdynamix.de>
2026-03-29 19:57:17 +0000

    add build_event_query

--- src/foxflow/utils/__init__.py
+++ src/foxflow/utils/__init__.py
@@ -2,3 +2,4 @@ from .time import get_timestamp_ns
 from .images import decode_ros_image, decode_jpeg
 from .sync import sync_dataframes
 from .paths import _safe_topic_name
+from .events import build_event_query
